c69de667b048ed2a21ca4634b9cf8b17fdf47183
Bernardo2209 <202501531768@alunos.estacio.br>
2025-03-03 17:23:36 +0000

    Desafio super trunfo

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,37 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(){
+int main () {
 
-printf ("Desafio do xadrez! \n");
+char estado[10];
+char codigo[10];
+char cidade [10];
 
+int populacao;
+int turisticos;
 
+float area;
+float PIB
 
+printf ("digite o estado: \n");
+scanf ("%s", &estado);
 
+printf ("digite o codigo: \n");
+scanf ("%s", &codigo);
 
+printf ("digite a cidade: \n");
+scanf ("%s", &cidade);
+
+printf("digite a populacao: \n");
+scanf ("%d", &populacao);
+
+printf ("digite os turisticos: \n");
+scanf ("%d", &turisticos);
+
+printf ("digite a area: \n");
+scanf ("%f", &area);
+
+printf ("digite o PIB: \n");
+scanf ("%f", &PIB)
+
+return 0;
 }
