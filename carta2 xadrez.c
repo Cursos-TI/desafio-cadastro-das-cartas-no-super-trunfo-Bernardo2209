179899722210545f93120fc9e3b91baf34e718bd
Bernardo2209 <202501531768@alunos.estacio.br>
2025-03-03 17:39:22 +0000

    cartaA02

---
+++ carta2 xadrez.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+
+int main () {
+
+char estado[10];
+char codigo[10];
+char cidade [10];
+
+int populacao;
+int turisticos;
+
+float area;
+float PIB;
+
+printf ("digite o estado: \n");
+scanf ("%s", &estado);
+
+printf ("digite o codigo: \n");
+scanf ("%s", &codigo);
+
+printf ("digite a cidade: \n");
+scanf ("%s", &cidade);
+
+printf("digite a populacao: \n");
+scanf ("%d", &populacao);
+
+printf ("digite os turisticos: \n");
+scanf ("%d", &turisticos);
+
+printf ("digite a area: \n");
+scanf ("%f", &area);
+
+printf ("digite o PIB: \n");
+scanf ("%f", &PIB);
+}
